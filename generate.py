c059f0d25debf697d754c3353ec29d8259669f09
regulus79 <117475203+regulus79@users.noreply.github.com>
2024-01-22 13:29:19 -0500

    Replace the nested loop with more numpy

--- generate.py
+++ generate.py
@@ -51,32 +51,26 @@ else:
     # If no pattern path was provided, then generate random noise as the pattern
     pattern=rng.random((random_pattern_width,height))
 
-# Helper functions
-def depth(x,y):
-    return 1 - imgdata[y,x]*depth_scale
-
-def get_pattern_at_point(x,y):
-    return pattern[round(x)%pattern.shape[0],y%pattern.shape[1],:]
-
-# Return the pattern if x<0. This essentially seeds the image with the pattern outside of view on the left; the pattern may get distorted as the image is constructed from left-to-right.
-def get_data_at_pos(data,x,y):
-    if x<0:
-        return get_pattern_at_point(x,y)
-    else:
-        return data[round(x),y,:]
-
-# Generate the output image data (TODO: Make this pure numpy and remove the nested for-loops)
+# Generate the output image data left-to-right
 data=np.zeros((width,height,3))
 for x in range(width):
-    for y in range(height):
-        # Calculate the position which the other eye should be guided to in order to match the depth
-        other_eye_pos=x-depth(x,y)*pattern.shape[0]
-        # Set the current pixel to be the same as the pixel which the other eye is looking at.
-        data[x,y]=get_data_at_pos(data,other_eye_pos,y)
+    # Calculate the position which the other eye should be guided to in order to match the depth
+    # - Invert the depth using (1 - pixel value) so that 0 is close and 1 is far.
+    # - Subtract the inverted_depth*depth_scale*pattern_width from the current x value to get the position where the other eye should be looking.
+    other_eye_pos=(x-(1 - imgdata[:,x]*depth_scale)*pattern.shape[0]).astype("int32")
+
+     # Set the current pixel to be the same as the pixel which the other eye is looking at.
+     # Set it to the pattern if it is refering to a point off the image<0. This essentially seeds the image with the pattern outside of view on the left; the pattern may get distorted as the image is constructed from left-to-right.
+     # Is is likely that for most cases all of the other_eye_pos's will either be on the image or off the image. However, it is possible that complicated scenes may have some on/off the image, depending on whether there exists an object near the left edge of the image.
+    indicies_off_image=other_eye_pos<0
+    indicies_on_image=~indicies_off_image
+    data[x,indicies_off_image]=pattern[other_eye_pos[indicies_off_image]%pattern.shape[0],np.arange(height)[indicies_off_image]%pattern.shape[1],:]
+    data[x,indicies_on_image]=data[other_eye_pos[indicies_on_image],np.arange(height)[indicies_on_image],:]
 
 # Save image to file
 output_img=Image.fromarray(data.transpose(1,0,2).astype("uint8"))
 output_img.save(output_path)
 
+# For debug purposes, show the image right away in a pyplot window. Will be removed in future versions.
 plt.imshow(data.transpose(1,0,2).astype("uint8"),interpolation="nearest")
 plt.show()
